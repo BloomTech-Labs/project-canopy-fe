3cb0aa7b4fd56be60a5309a12973d843d529a935
Eric Ferguson <turtle0888@gmail.com>
2020-04-09 11:52:19 -0500

    Modified package.json

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import { Route, Switch } from 'react-router-dom';
-import {Layout, Row, Col} from 'antd';
+import { Layout, Row, Col } from 'antd';
 import styled from 'styled-components';
 
-import {Dashboard} from './components/Dashboard.js';
-import {Nav} from './components/Nav'
-
+import { Dashboard } from './components/Dashboard.js';
+import { Nav } from './components/Nav';
 
 function App() {
   return (
-    
-      <Layout>
-          <Nav />
-          <Layout style={{backgroundColor:'#F0F0F0', height:'100vh'}}>      
-              
-                <Dashboard />
-              
-          </Layout> 
-
-      </Layout> 
- 
-    
+    <Layout>
+      <Nav />
+      <Layout style={{ backgroundColor: '#F0F0F0', height: '100vh' }}>
+        <Dashboard />
+      </Layout>
+    </Layout>
   );
 }
 
 export default App;
-
