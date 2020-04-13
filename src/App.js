bff87848be6462edb03e67d9e0589cf868239d8d
Eric Ferguson <turtle0888@gmail.com>
2020-04-13 11:01:38 -0500

    Changed dashboard to lowercase

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import { Route, Switch } from 'react-router-dom';
-import {Layout} from 'antd';
+import { Layout } from 'antd';
 
-import {Dashboard} from './components/Dashboard.js';
-import {Nav} from './components/Nav'
-
+import { Dashboard } from './components/dashboard.js';
+import { Nav } from './components/Nav';
 
 function App() {
   return (
-    
     <Layout>
-        <Nav />
+      <Nav />
 
-        <Layout style={{backgroundColor:'#F0F0F0', height:'100vh'}}>
-          <Switch>
-            
-            <Route>
-              <Dashboard /> 
-            </Route>
-
-          </Switch>                    
-        </Layout> 
-
-    </Layout> 
- 
-    
+      <Layout style={{ backgroundColor: '#F0F0F0', height: '100vh' }}>
+        <Switch>
+          <Route>
+            <Dashboard />
+          </Route>
+        </Switch>
+      </Layout>
+    </Layout>
   );
 }
 
 export default App;
-
