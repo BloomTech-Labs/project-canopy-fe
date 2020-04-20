cefa357d3ad97019dcc4db57a448350ad3d7a08e
Chris Engel <Chris.Engel@yahoo.com>
2020-04-20 15:20:04 -0400

    removed old unused redux code

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import { chartReducer } from './chart_reducer.js';
 import { tableReducer } from './table_reducer.js';
-import { reducer as original_reducer } from './original_reducer.js';
 
 export default combineReducers({
     chartReducer,
     tableReducer,
-    original_reducer
 })
